2232e7349d29c01de6bd0c315f7f8858c30f2529
ishantchauhan710 <ishantchauhan710@gmail.com>
2024-09-12 21:22:36 +0530

    Theme setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,26 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        light: {
+          primary: "#F0F1F1",
+          variant: "#E5E5E5",
+        },
+        metal: {
+          primary: "#989BA6",
+          variant: "#6E7688",
+        },
+        ocean: {
+          primary: "#95BAE5",
+          variant: "#638AD1",
+        },
+        air: {
+          primary: "#DAE1E6",
+          variant: "#B1BDCB",
+        },
+        dark: {
+          primary: "#303343",
+          variant: "#272A37",
+        },
       },
     },
   },
